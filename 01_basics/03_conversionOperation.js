e37b0f639a7b673fbb8180c64c40c45886ac22f5
Mohammad Salman Raza <135103272+SalmanRaza03@users.noreply.github.com>
2024-03-02 17:41:18 +0000

    TypeConversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+/*let score = "45";
+
+let numberedScore = Number(score);
+
+console.log(typeof numberedScore);
+console.log(numberedScore);
+*/
+
+/*let isLoggedIn = "Salman";
+let checkIsLoggedIn = Number(isLoggedIn);
+console.log(typeof checkIsLoggedIn);
+console.log(checkIsLoggedIn);*/
+
+/*let isLoggedIn = "";
+let checkIsLoggedIn = Boolean(isLoggedIn);
+console.log(typeof checkIsLoggedIn);
+console.log(checkIsLoggedIn);
+*/
+
+let isLoggedIn = undefined;
+let checkIsLoggedIn = String(isLoggedIn);
+
+console.log(typeof checkIsLoggedIn);
+console.log(checkIsLoggedIn);
