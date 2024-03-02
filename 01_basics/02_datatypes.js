895bd3a33dea2044f3b818b8ad158e02ba0ce804
Mohammad Salman Raza <135103272+SalmanRaza03@users.noreply.github.com>
2024-03-02 06:59:57 +0000

    Learnt DataTypes in Javascript

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"
+
+// try{
+//     alert("This is an alert message");
+// }catch(e){
+//     console.log("Error Occurred", e.message);
+// }
+// console.log("Hello");
+// console.log("Heya")
+
+/*
+    number, bigint
+    string,
+    boolean => basically they have true and false values,
+    symbol => used mainly in react when you need to identify the uniquety of the components,
+    null,
+    undefined
+*/
+let variable = 45;
+console.log(typeof null);//object
+console.log(typeof undefined)//undefined
+
+console.log("Learnt Datatypes");
