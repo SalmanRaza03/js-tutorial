450480e91ae2b04d050bb5bfb9c91409a92a7d92
Mohammad Salman Raza <135103272+SalmanRaza03@users.noreply.github.com>
2024-03-02 06:25:08 +0000

    Learnt Variables in JavaScript

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 23;
+let accountHolderName = "Himesh";
+var accountCity = "Ooty";
+accountNominee = "Hitesh";
+
+/*
+console.log(accountState);
+let accountState = 10;
+console.log(accountState);
+*/
+
+console.log(accountId)
+console.table([accountId, accountHolderName, accountCity, accountNominee]);
