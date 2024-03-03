67d23e8d76980a1e4c013be9b63461afbcef9229
Mohammad Salman Raza <135103272+SalmanRaza03@users.noreply.github.com>
2024-03-03 07:48:15 +0000

    Learnt how Type Conversions work with operations.

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -17,8 +17,25 @@ console.log(typeof checkIsLoggedIn);
 console.log(checkIsLoggedIn);
 */
 
-let isLoggedIn = undefined;
-let checkIsLoggedIn = String(isLoggedIn);
+// let isLoggedIn = undefined;
+// let checkIsLoggedIn = String(isLoggedIn);
 
-console.log(typeof checkIsLoggedIn);
-console.log(checkIsLoggedIn);
+// console.log(typeof checkIsLoggedIn);
+// console.log(checkIsLoggedIn);
+
+//console.log("1" + 2 + 2);
+
+//console.log(2+2+"1");
+
+/*let str1 = "Salman";
+let str2 = "Sal";
+
+let str3 = str1 - str2;//It typecasts into a number due to the presence of the minus operator and str3 is NaN.
+console.log(typeof str3);
+console.log(str3);*/
+
+let num = 32;
+let num1 = ++num;
+console.log(typeof num1);
+console.log(num1);
+
