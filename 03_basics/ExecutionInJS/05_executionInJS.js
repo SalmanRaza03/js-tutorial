7075e79c80221761a223703dacc87a3ae51d937f
Mohammad Salman Raza <135103272+SalmanRaza03@users.noreply.github.com>
2024-03-11 05:46:11 +0000

    How Code is executed in JS and Function Call Stack

---
+++ 03_basics/ExecutionInJS/05_executionInJS.js
@@ -0,0 +1,14 @@
+//How are the programs executed in javascript?
+
+// ------------------------------------------JAVASCRIPT EXECUTION CONTEXT------------------------------------
+
+
+//whenever a code is executed in javascript, it is done in mainly two execution contexts,
+// 1- Global execution context and 2- Functional execution context.
+//While dealing with other technologies like Mongoose there is another context called as - "Eval Execution Context"
+
+// The Global execution context deals with the global object handled by the "this" keyword. It is different in various contexts like browser, node, deno, and bun. In the browser, the global object is the "Window" object.
+
+//whenever a code is executed in javascript, it is done in two phases namely
+//1. Memory Creation Phase or simply Creation Phase
+//2. Execution Phase
