d8073557b3bb054cb06d44a1f933575e72c2a451
Mohammad Salman Raza <135103272+SalmanRaza03@users.noreply.github.com>
2024-03-16 06:52:54 +0000

    Revised TypeConversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,38 +1,39 @@
-/*let score = "45";
+let score = "45";
 
 let numberedScore = Number(score);
+// console.log(score)//45
+// console.log(typeof score);//string
+// console.log(typeof numberedScore);//number
+// console.log(numberedScore);//45
 
-console.log(typeof numberedScore);
-console.log(numberedScore);
-*/
 
 /*let isLoggedIn = "Salman";
 let checkIsLoggedIn = Number(isLoggedIn);
-console.log(typeof checkIsLoggedIn);
-console.log(checkIsLoggedIn);*/
+console.log(typeof checkIsLoggedIn);//number
+console.log(checkIsLoggedIn);*///NaN
 
-/*let isLoggedIn = "";
-let checkIsLoggedIn = Boolean(isLoggedIn);
-console.log(typeof checkIsLoggedIn);
-console.log(checkIsLoggedIn);
-*/
+// let isLoggedIn = "";
+// let checkIsLoggedIn = Boolean(isLoggedIn);
+// console.log(typeof checkIsLoggedIn);//boolean
+// console.log(checkIsLoggedIn);//false
+
 
 // let isLoggedIn = undefined;
 // let checkIsLoggedIn = String(isLoggedIn);
 
-// console.log(typeof checkIsLoggedIn);
-// console.log(checkIsLoggedIn);
+// console.log(typeof checkIsLoggedIn);//string
+// console.log(checkIsLoggedIn);//undefined
 
-//console.log("1" + 2 + 2);
+// console.log("1" + 2 + 2);//122
 
-//console.log(2+2+"1");
+// console.log(2+2+"1");//41
 
-/*let str1 = "Salman";
-let str2 = "Sal";
+// let str1 = "Salman";
+// let str2 = "Sal";
 
-let str3 = str1 - str2;//It typecasts into a number due to the presence of the minus operator and str3 is NaN.
-console.log(typeof str3);
-console.log(str3);*/
+// let str3 = str1 - str2;//It typecasts into a number due to the presence of the minus operator and str3 is NaN.
+// console.log(typeof str3);
+// console.log(str3);
 
 let num = 32;
 let num1 = ++num;
