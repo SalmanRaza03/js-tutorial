683ae20a08d8041c4fdbb03951ea43a4ed8f0d08
Mohammad Salman Raza <135103272+SalmanRaza03@users.noreply.github.com>
2024-03-16 07:48:46 +0000

    Revised Strings

--- 01_basics/06_strings.js
+++ 01_basics/06_strings.js
@@ -5,7 +5,7 @@ const repoCount = 30;
 
 // console.log("My name is " + name + " and my repoCount is " + repoCount);
 
-// console.log(`My name is ${name} and my repo count is ${repoCount}`);
+// console.log(`My name is ${name} and my repo count is ${repoCount}`);//This is called as String Interpolation.
 
 const myName = new String('Salman Raza');
 // console.log(myName.toLowerCase());
@@ -15,15 +15,19 @@ const myName = new String('Salman Raza');
 // console.log(myName.indexOf("aza"));//8 is the output
 // console.log(myName.length);
 
-const myNewName = new String("SalmanRaza");
-// console.log(myNewName.substring(1,6));
+console.log(myName.__proto__);
 
-//usage of slice method in strings
-/*console.log(myNewName.slice(1,5));//alma
-console.log(myNewName.slice(5,-1));//nRaz
-console.log(myNewName.slice(-4,8));//Ra
-console.log(myNewName.slice(-4,-1));//Raz
-*/
+const myNewName = new String("SalmanRaza");
+// console.log(myNewName.substring(1,6));//(index, index-1). output is alman(index 1->5)
+
+// //usage of slice method in strings
+// console.log(myNewName.slice(5))//nRaza
+// console.log(myNewName.slice(-7));//manRaza
+// console.log(myNewName.slice(1,5));//alma
+// console.log(myNewName.slice(5,-1));//nRaz
+// console.log(myNewName.slice(-4,8));//Ra
+// console.log(myNewName.slice(-4,-1));//Raz
+
 
 //trim method
 const myString = "     Salman     ";
@@ -31,11 +35,11 @@ const myString = "     Salman     ";
 // console.log(myString.trim());
 
 const anotherString = "Salman Raza is currently unemployed";
-console.log(anotherString.replace('Raza', 'khan'));//only replaces the first occurrence of the given argument
-console.log(anotherString.replaceAll('Raza', 'Khan'));//replaces all the places having 'Raza'
-console.log(anotherString.includes('unemployed'));
+// console.log(anotherString.replace('Raza', 'khan'));//only replaces the first occurrence of the given argument
+// console.log(anotherString.replaceAll('Raza', 'Khan'));//replaces all the places having 'Raza'
+// console.log(anotherString.includes('unemployed'));
 
-console.log(anotherString.split(' '));
+// console.log(anotherString.split(' '));//returns an array based on the split value(here it is space(" "))
 
 //startsWith("Salman") and endsWith("Raza")------- The output is true for both of them, they return a boolean value
 
