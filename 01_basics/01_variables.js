ae693ca8795fd027821555aba4cf263a62865ad7
Mohammad Salman Raza <135103272+SalmanRaza03@users.noreply.github.com>
2024-03-16 06:40:06 +0000

    Revised Variables in javascript

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -3,11 +3,13 @@ let accountHolderName = "Himesh";
 var accountCity = "Ooty";
 accountNominee = "Hitesh";
 
+var accountCity = "Vizag";
+
 /*
 console.log(accountState);
 let accountState = 10;
 console.log(accountState);
 */
 
-console.log(accountId)
-console.table([accountId, accountHolderName, accountCity, accountNominee]);
+console.log(accountCity);
+// console.table([accountId, accountHolderName, accountCity, accountNominee]);
