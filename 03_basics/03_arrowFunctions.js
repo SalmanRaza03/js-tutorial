61daee8204a41276331eb959cb22e81604e65766
Mohammad Salman Raza <135103272+SalmanRaza03@users.noreply.github.com>
2024-03-17 07:07:27 +0000

    revised this keyword and arrow functions

--- 03_basics/03_arrowFunctions.js
+++ 03_basics/03_arrowFunctions.js
@@ -15,26 +15,29 @@ const user = {
 
 // function chai(){
 //     let userName = "salman"
-//     console.log(this.userName);//outputs undefined.
+//     console.log(this.userName);//outputs undefined bcoz In JavaScript, the this keyword refers to the context in which
+//                                  a function is executed. In this case, the function chai() is executed in the global context, 
+//                                  and within the global context, there is no userName property defined.
 // }
 // chai()
 
 // const variable = function(){
 //     let userName = "salman"
-//     console.log(this.userName);
+//     console.log(this.userName);//output is undefined for the same above reason
 // }
 // variable();
 
 
 // ------------------------ARROW FUNCTIONS------------------------------
 
-// const addTwoNumbers = (num1, num2) => {//when you use {}, you have to return something explicitly for sure.
+// const addTwoNumbers = (num1, num2) => {       //when you use {}, you have to return something explicitly for sure.
 //     return num1 + num2;
 // }
 // console.log(addTwoNumbers(3,4));
 
-// const addTwoNumbers = (num1, num2) => num1 + num2//here no need to write return as it is considered a single exp. You can wrap with ()
-// const addTwoNumbers = (num1, num2) => (num1 + num2)
+// const addTwoNumbers = (num1, num2) => num1 + num2//here no need to write return as it is considered a single exp. You can
+//                                                    wrap with ()
+// const addTwoNumbers = (num1, num2) => (num1 + num2)//like this
 // console.log(addTwoNumbers(3,4));
 
 
